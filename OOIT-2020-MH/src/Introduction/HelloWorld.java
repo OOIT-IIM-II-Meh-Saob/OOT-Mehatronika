85a5d70968475c9caf4de67d74a483a91a7ca9af
Andrej Katin <katin.andrej96@gmail.com>
2020-10-30 20:16:22 +0100

    Vezbe 1 - dopuna

--- OOIT-2020-MH/src/Introduction/HelloWorld.java
+++ OOIT-2020-MH/src/Introduction/HelloWorld.java
@@ -5,6 +5,58 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// Ovo je komentar
 		System.out.println("Hello, World!");
+		
+		// PROMENLJIVE
+		int firstNumber = 4;
+		double secondNumber = 4.4;
+		
+		double sum = firstNumber + secondNumber;
+		System.out.println(sum);
+		
+		char c = ' ';
+		String firstName = "Petar";
+		String lastName = "Petrovic";
+		System.out.println("Student: " + firstName + c + lastName);
+		
+		boolean bool = true;
+		
+		// USLOVI
+		int a = 24;
+		int b = 12;
+		char operation = '*';
+		
+		int result = -1;
+		
+		if (operation == '+') {
+			result = a + b;
+		}
+		else if (operation == '-') {
+			if (a > b) {
+				result = a - b;
+			} else {
+				result = b - a;
+			}
+		}
+		else if (operation == '*') {
+			result = a * b;
+		}
+		else if (operation == '/') {
+			result = a / b;
+		}
+		else {
+			System.out.println("Nedozvoljeni unos!");
+		}
+		
+		System.out.println("Rezultat je: " + result);
+		
+		// PETLJE
+		int f = 5;
+		int sumF = 1;
+		while (f > 0) {
+			sumF = sumF * f;
+			f--;
+		}
+		System.out.println("Faktorijel broja 5 je: " + sumF);
 	}
 
 }
